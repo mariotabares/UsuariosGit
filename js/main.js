ce5f0c76e4a665b958c9f5b44948b43e0fc8d707
Mario <alejndro.tabares@udea.edu.co>
2023-02-20 12:06:10 -0500

    new file:   css/main.css 	new file:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const API = "https://api.github.com/users/";
+
+
+//INTANCIA DE VUE SIN HERRAMIENTA DE EMPAQUETADO
+//ESTRUCTURA
+const app = Vue.createApp({
+    //funcion de valores
+    data() {
+        return {
+            search: null,
+            result: null,
+            error: null,
+        }
+    },
+    //METODOS
+    methods: {
+        //Busqueda
+        async doSearch() {
+            //Estructura para mostar error con limpiado de caja
+            this.result = this.error = null
+            try {
+                const response = await fetch(API + this.search)
+                if (!response.ok) throw new Error("User no fount")
+                const data = await response.json()
+                console.log(data)
+                this.result = true
+            } catch (error) {
+                this.error = error
+            } finally {
+                this.search = null
+            }
+        }
+    }
+})
