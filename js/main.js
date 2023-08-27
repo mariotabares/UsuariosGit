eb2857e8d3e2ac554dec6138d74067d21b88c257
Mario Tabares <alejndro.tabares@udea.edu.co>
2023-08-27 17:16:12 -0500

    Solucion De Favoritos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//El endpoint de la API de GitHub es https://api.github.com/users/
+
 const API = "https://api.github.com/users/";
 
 
@@ -13,18 +15,29 @@ const app = Vue.createApp({
             favorites: new Map()
         }
     },
+    //FUNCIONES COMPUTADAS
+    computed: {
+        //Funcion para mostrar el resultado de la busqueda
+        isFavorite() {
+            return this.favorites.has(this.result && this.result.id)
+        }
+    },
     //METODOS
+    // En vue los metodos deben ir dentro de la funcion 
     methods: {
         //Busqueda
         async doSearch() {
             //Estructura para mostar error con limpiado de caja
             this.result = this.error = null
             try {
+                //Estructura para mostrar la informacion formato JSON
                 const response = await fetch(API + this.search)
                 if (!response.ok) throw new Error("User no fount")
+                //Descomprime el JSON
                 const data = await response.json()
                 console.log(data)
                 this.result = data
+
             } catch (error) {
                 this.error = error
             } finally {
@@ -33,6 +46,9 @@ const app = Vue.createApp({
         },
         addFavorite(){
             this.favorites.set(this.result.id,this.result)
+        },
+        removeFavorite(){
+            this.favorites.delete(this.result.id)
         }
     }
 })
