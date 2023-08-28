1d53258376f91479d9e1dd03a9bf1deddac12521
Mario Tabares <alejndro.tabares@udea.edu.co>
2023-08-28 12:44:51 -0500

    configuracion antes de los componentes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 const API = "https://api.github.com/users/";
 
+//Tiempo maximo de respuesta a la peticion
+
+const requestMaxTimeMs=2000000
 
 //INTANCIA DE VUE SIN HERRAMIENTA DE EMPAQUETADO
 //ESTRUCTURA
@@ -20,7 +23,7 @@ const app = Vue.createApp({
     created(){
         const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
         if( savedFavorites?.length){
-            const favorites= new Map(savedFavorites.map(favorite=>[favorite.id,favorite]))
+            const favorites= new Map(savedFavorites.map(favorite=>[favorite.login,favorite]))
             this.favorites=favorites
         }
         
@@ -29,7 +32,7 @@ const app = Vue.createApp({
     computed: {
         //Funcion para mostrar el resultado de la busqueda
         isFavorite() {
-            return this.favorites.has(this.result && this.result.id)
+            return this.favorites.has(this.result.login)
         }
         ,
         //Funcion para mostrar los favoritos
@@ -44,7 +47,30 @@ const app = Vue.createApp({
         async doSearch() {
             //Estructura para mostar error con limpiado de caja
             this.result = this.error = null
+
+            //forma de no tener la misma peticion una y otra vez
+            const foundInFavorites = this.favorites.get(this.search)
+
+            //programando un tiempo maximo de respuesta a la peticion 
+            const  shouldRequestAgain=(()=>{
+                if(!!foundInFavorites){
+                    const {lastRequestTime}=foundInFavorites
+                    const now = Date.now()
+                    return (now-lastRequestTime)<requestMaxTimeMs
+                }
+                return false
+            })() //funcion autoejecutable (IIFE)
+
+            console.log(!shouldRequestAgain)
+
+            if(!!foundInFavorites && shouldRequestAgain){
+                console.log("Usando datos de cache")
+                return this.result = foundInFavorites
+
+            }
+
             try {
+                console.log("Haciendo peticion")
                 //Estructura para mostrar la informacion formato JSON
                 const response = await fetch(API + this.search)
                 if (!response.ok) throw new Error("User no fount")
@@ -52,6 +78,7 @@ const app = Vue.createApp({
                 const data = await response.json()
                 console.log(data)
                 this.result = data
+                this.foundInFavorites.lastRequestTime = Date.now()
 
             } catch (error) {
                 this.error = error
@@ -60,17 +87,24 @@ const app = Vue.createApp({
             }
         },
         addFavorite(){
-            this.favorites.set(this.result.id,this.result)
+
+            this.result.lastRequestTime=Date.now()
+            this.favorites.set(this.result.login,this.result)
             this.updateStorage()
         },
         removeFavorite(){
-            this.favorites.delete(this.result.id)
+            this.favorites.delete(this.result.login)
             this.updateStorage()
         },
         //mostrar favoritos ya guardados 
         showFavorite(favorite){
             this.result=favorite
         },
+        //Verificar si el usuario es favorito
+        checkFavorite(id){
+            return this.result?.login===id
+        },
+
         // funcion persistencia de datos en el local storage
         updateStorage(){
             window.localStorage.setItem("favorites",JSON.stringify(this.allFavorites))
